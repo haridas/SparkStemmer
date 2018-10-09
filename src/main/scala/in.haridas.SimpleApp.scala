8dce8a5a2b3c664a6af4cdcb70983eac738f44db
Haridas N <haridas.nss@gmail.com>
2018-10-09 17:33:30 +0530

    Porter stemming spark pipeline.
    Using this library https://github.com/master/spark-stemming with
    scala 2.11 and spark 2.3.2 version.

--- src/main/scala/in.haridas.SimpleApp.scala
+++ src/main/scala/in.haridas.SimpleApp.scala
@@ -2,12 +2,23 @@ import org._
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.feature.{NGram, Tokenizer}
 import org.apache.spark.mllib.feature.Stemmer
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.functions._
 import org.tartarus._
 import org.tartarus.snowball._
 
+import scala.tools.nsc.transform.Transform
+
+
+
 /* SimpleApp.scala */
+
+case class Data(value: String, id: Int, stemmed: String)
+
+case class Word(id: Long, word: String, stemmed: String)
 
 object SimpleApp {
   def main(args: Array[String]) {
@@ -24,21 +35,52 @@ object SimpleApp {
 
 
 //    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
-    val df = SparkSession.builder().getOrCreate();
+    val sparkSession = SparkSession.builder().getOrCreate();
+    // For implicit conversions like converting RDDs to DataFrames
+    import sparkSession.implicits._
+    // For implicit conversions like converting RDDs to DataFrames
     // this is used to implicitly convert an RDD to a DataFrame.
 
-    val data = df.createDataFrame(
-      Seq(("testing", 1, ""), ("test", 2, ""), ("tester", 3, ""))
-    ).toDF("word", "id", "stemmed")
+
+//    val data = sparkSession.createDataFrame(Seq( Seq("testing", 1, ""), Seq("test", 2, ""), Seq("tester", 3, "")).toDF()
+
+
+    //val data = Seq("test", "testing", "tester").toDS()
+
+    val dataset = Seq(Word(0, "hello", ""), Word(1, "spark", "")).toDS
+
+    val datasetDf = sparkSession.createDataFrame(Seq(
+      (1L, "haridas close, closing closer closed the document... testing"),
+      (2L, "Module was tested by Amos"),
+      (3L, "Tested by Haridas")
+    )).toDF("id", "word")
+
+
+    val tokenizer = new Tokenizer()
+      .setInputCol("word")
+      .setOutputCol("tokens")
 
     val stemmer = new Stemmer()
-      .setInputCol("word")
+      .setInputCol("tokens")
       .setOutputCol("stemmed")
       .setLanguage("english")
-      .transform(data)
 
-    stemmer.show()
+    val pipeline = new Pipeline()
+      .setStages(Array(tokenizer, stemmer))
+    val model = pipeline.fit(datasetDf)
 
-    data.show()
+    model.transform(datasetDf)
+      .select("id", "tokens", "stemmed")
+      .collect()
+      .foreach{ case Row(id: Long, tokens: Array[String], stemmed: Array[String]) =>
+          println(s"ID: $id , tokens: $tokens , stemmed: $stemmed")
+      }
+
+
+
+//    model.write.overwrite().save("/tmp/stemmer.log")
+
+
+//    dataset.show()
   }
 }
